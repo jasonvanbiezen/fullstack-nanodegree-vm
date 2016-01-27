6f1b0960b1f05a21b383af4a5db245082765a64a
Jason van Biezen <jason.vanbiezen@gmail.com>
2016-01-27 10:02:27 -0800

    fixed webserver post

--- vagrant/webserver/webserver.py
+++ vagrant/webserver/webserver.py
@@ -1,6 +1,8 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
 
 class WebserverHandler(BaseHTTPRequestHandler):
+    #messageForm = """<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?<h2><input name='message' type='text'><input type='submit' value='Submit'></form>"""
     def do_GET(self):
         try:
             if self.path.endswith('/hello'):
@@ -8,23 +10,40 @@ class WebserverHandler(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 output = ""
-                output += """<html><body><b>hi!</b><a href="/theforceawakens">Use the force Rae!</a></body><html>"""
+                output += """<html><body><b>hi!</b><a href="/theforceawakens">Use the force Rae!</a>"""
+                output += """<form method='POST' enctype='multipart/form-data' action='/'><h2>What would you like me to say?<h2><input name='message' type='text'><input type='submit' value='Submit'></form>"""
+                output += """</body><html>"""
                 self.wfile.write(output)
             elif self.path.endswith('/theforceawakens'):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 output = ""
-                output += """<html><body><b>Awakens the force does!</b><br><a href="/hello">To hello page</a></body><html>"""
-                self.wfile.write(output)
-            else:
-                self.send_response(404)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                output = """<html><body><h1>404 error</h1></body></html>"""
+                output += """<html><body><b>Awakens the force does!</b><br><a href="/hello">To hello page</a>"""
+                output += """<form method='POST' enctype='multipart/form-data' action='/'><h2>What would you like me to say?<h2><input name='message' type='text'><input type='submit' value='Submit'></form>"""
+                output += """</body><html>"""
                 self.wfile.write(output)
         except:
             self.send_error(404, "File not found %s" % self.path)
+    def do_POST(self):
+        try:
+            self.send_response(301)
+            self.end_headers()
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields=cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+                output = ""
+                output +=  "<html><body>"
+                output += " <h2> Okay, how about this: </h2>"
+                output += "<h1> %s </h1>" % messagecontent[0]
+                output += """<form method='POST' enctype='multipart/form-data' action='/'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"></form>"""
+                output += "</html></body>"
+            else:
+                output="<html><body><h1>no message</h1></body></html>"
+            self.wfile.write(output)
+        except:
+            pass
 
 
 def main():
