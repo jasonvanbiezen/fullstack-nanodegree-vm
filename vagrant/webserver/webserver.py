596757965113b7b36abfa15d32402f5550aa7dcd
Jason van Biezen <jason.vanbiezen@gmail.com>
2016-02-03 23:06:27 -0800

    added restaurant delete

--- vagrant/webserver/webserver.py
+++ vagrant/webserver/webserver.py
@@ -11,13 +11,17 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
+
+force_url = '/theforceawakens'
+multipart_form_data = 'multipart/form-data'
+
 template = Template("""
 <html><body><a href="{{url}}">{{link_message}}</a>
-<form method='POST' enctype='multipart/form-data' action='/'>
+<form method='POST' enctype='%s' action='/'>
 <h2>You said: {{message}}</h2>
 <h2>What would you like me to say?<h2><input name='message' type='text'><input type='submit' value='Submit'></form>
 </body></html>
-""")
+""" % multipart_form_data )
 
 restaurantTemplate = Template("""
 <html><body>
@@ -37,20 +41,27 @@ restaurantTemplate = Template("""
 
 restaurantEditTemplate = Template("""
 Change the name of restaurant {{id}}: <b>{{name}}</b><br />
-<form method='POST' enctype='multipart/form-data' action='{{action_url}}'>
+<form method='POST' enctype='%s' action='{{action_url}}'>
 <input name='new_name' type='text'><br />
 <input type='submit' value='Confirm'>
 </form>
-""")
+""" % multipart_form_data)
+
+restaurantDeleteTemplate = Template("""
+Are you sure you want to delete restaurant: <b>{{name}}</b><br />
+<form method='POST' enctype='%s' action='{{action_url}}'>
+<input type='submit' value='Delete'>
+</form>
+""" % multipart_form_data)
 
 newRestaurantTemplate = Template("""
 Create a new restaurant: 
-<form method='POST' enctype='multipart/form-data' action='/restaurants'>
+<form method='POST' enctype='%s' action='/restaurants'>
 <input name='new_name' type='text'>
 <input type='submit' value='Submit'>
 </form>
 <br />
-""")
+""" % multipart_form_data)
 
 restaurantRenamedTemplate = Template("""
 <html><body>
@@ -58,10 +69,7 @@ Restaurant renamed successfully!
 </body></html>
 """)
 
-force_url = '/theforceawakens'
-
 class WebserverHandler(BaseHTTPRequestHandler):
-    #messageForm = """<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?<h2><input name='message' type='text'><input type='submit' value='Submit'></form>"""
     def do_GET(self):
         try:
             path_parts = self.path.split('?')
@@ -103,6 +111,17 @@ class WebserverHandler(BaseHTTPRequestHandler):
                 restaurants = session.query(Restaurant).all()
                 content = restaurantEditTemplate.render(name=restaurant_name, id=restaurant_id, action_url=url_path)
                 self.wfile.write(restaurantTemplate.render(content=content, restaurants=restaurants))
+            elif url_path.startswith('/restaurants') and url_path.endswith('/delete'):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                url_comp = url_path.split('/')
+                restaurant_id = url_comp[2]
+                r = session.query(Restaurant).filter(Restaurant.id==int(restaurant_id)).first()
+                restaurants = session.query(Restaurant).all()
+                r_name = r.name if r is not None else ''
+                content = restaurantDeleteTemplate.render(name=r_name, action_url=url_path)
+                self.wfile.write(restaurantTemplate.render(content=content, restaurants=restaurants))
             else:
                 self.send_response(404)
                 self.send_header('Content-type', 'text/html')
@@ -120,7 +139,7 @@ class WebserverHandler(BaseHTTPRequestHandler):
             
             if url_path.startswith('/restaurants') and url_path.endswith('/edit'):
                 ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-                if ctype == 'multipart/form-data':
+                if ctype == multipart_form_data:
                     fields=cgi.parse_multipart(self.rfile, pdict)
                     new_name = fields.get('new_name')
                     r_id = url_path.split('/')[2]
@@ -133,7 +152,22 @@ class WebserverHandler(BaseHTTPRequestHandler):
                     self.end_headers()
                 else:
                     raise ValueError("Invalid restaurant name.")
-            elif self.path.endswith('/restaurants'):
+            elif url_path.startswith('/restaurants') and url_path.endswith('/delete'):
+                r_id = url_path.split('/')[2]
+                r = session.query(Restaurant).filter(Restaurant.id==r_id).first()
+                if r != None:
+                    content = "Restuarant %s deleted!" % r.name
+                    session.query(Restaurant).filter(Restaurant.id==r_id).delete()
+                    session.commit()
+                else :
+                    content = "Cannot delete: No such restaurant!"
+                restaurants = session.query(Restaurant).all()
+                #self.send_response(301)
+                #self.send_header('Location', '/restaurants')
+                #self.send_header('Content-type', 'text/html')
+                #self.end_headers()
+                self.wfile.write(restaurantTemplate.render(restaurants=restaurants, content=content))
+            elif url_path.endswith('/restaurants'):
                 self.send_response(301)
                 self.end_headers()
                 ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
